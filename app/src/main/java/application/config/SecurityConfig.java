263ba8494e7b6671b874e998afb1dc5111d88abb
Isabelly Miranda <contatoisabellymiranda@gmail.com>
2024-06-11 17:23:50 +0000

    Atualizar

--- app/src/main/java/application/config/SecurityConfig.java
+++ app/src/main/java/application/config/SecurityConfig.java
@@ -28,8 +28,11 @@ public class SecurityConfig {
                     .sessionManagement()
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                     .and().authorizeHttpRequests()
-                    .requestMatchers(HttpMethod.POST, "/auth").permitAll()
-                    .anyRequest().authenticated()
+                    // .requestMatchers(HttpMethod.POST, "/auth").permitAll()
+                    // .requestMatchers(HttpMethod.GET,"/swagger-ui/*").permitAll()
+                    // .requestMatchers(HttpMethod.GET,"/v3/api-docs/**").permitAll()
+                    // .anyRequest().authenticated()
+                    .anyRequest().permitAll()
                     .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                     .build();
     }
